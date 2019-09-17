fb0f7508c71fac5d33048a217991f9eaff34f4b0
xy15984 <154495967@qq.com>
2019-09-17 17:01:16 +0800

    Add files via upload

---
+++ files/ggad2_728x90.js
@@ -0,0 +1,12 @@
+document.writeln("<link type=\'text/css\' rel=\'stylesheet\' href=\'http://www.cssmoban.com/statics/css/astyle.css\' />");
+document.writeln("<div align=\'center\'>");
+document.writeln("<script async src=\'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<!-- 970-90 -->");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:inline-block;min-width:400px;max-width:1200px;width:100%;height:90px\'");
+document.writeln("     data-ad-client=\'ca-pub-1542822386688301\'");
+document.writeln("     data-ad-slot=\'3747595052\'></ins>");
+document.writeln("<script>");
+document.writeln("     (adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
+document.writeln("</div>");
